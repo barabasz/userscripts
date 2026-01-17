4196ea65b17220784e3a913357859eba6177f286
Andrzej Barabasz <abarabasz@gmail.com>
2026-01-17 21:13:41 +0100

    update

--- wiki-remove-references.user.js
+++ wiki-remove-references.user.js
@@ -16,20 +16,41 @@
 
 (function() {
     'use strict';
-    const content = document.querySelector('div#mw-content-text');
-    if (content) {
+
+    function removeReferences() {
+        const content = document.querySelector('div#mw-content-text');
+        if (!content) return;
+
         // Remove reference markers
         document.querySelectorAll('sup.reference').forEach(el => el.remove());
 
-        // Remove references section and its heading
-        let refSection = content.querySelector('div.refsection, div.mw-references-wrap, div.reflist');
-        if (refSection) {
-            const prevElement = refSection.previousElementSibling;
-            if (prevElement && prevElement.classList.contains('mw-heading') &&
-                prevElement.classList.contains('mw-heading2')) {
-                prevElement.remove();
+        // Find and remove References heading by h2 id
+        const refHeadings = content.querySelectorAll('h2#References, h2#Przypisy, h2#Notes');
+        refHeadings.forEach(h2 => {
+            // Remove the parent mw-heading div
+            const headingDiv = h2.closest('div.mw-heading');
+            if (headingDiv) {
+                headingDiv.remove();
             }
-            refSection.remove();
-        }
+        });
+
+        // Remove references section content
+        content.querySelectorAll('div.refsection, div.mw-references-wrap, div.reflist').forEach(el => el.remove());
+    }
+
+    // Initial cleanup
+    removeReferences();
+
+    // Observe for dynamically loaded content
+    const content = document.querySelector('div#mw-content-text');
+    if (content) {
+        const observer = new MutationObserver(() => {
+            removeReferences();
+        });
+
+        observer.observe(content, {
+            childList: true,
+            subtree: true
+        });
     }
 })();
